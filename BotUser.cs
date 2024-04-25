eeaa22ee8a78ce4caf202b05225321d43a4584eb
Mako864 <165297903+BisocM@users.noreply.github.com>
2024-04-25 19:36:51 +0100

    Repository Initialization

---
+++ BotUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDiscordSubtitlingBot
+{
+    internal class BotUser
+    {
+        private static readonly Lazy<BotUser> lazy = new(() => new BotUser());
+
+        public static BotUser Instance { get { return lazy.Value; } }
+
+        private BotUser() { }
+
+        public string DiscordBotToken { get; set; }
+        public string DiscordClientId { get; set; }
+        public string MicrosoftCognitiveServicesKey { get; set; }
+    }
+}
---
+++ SpeechRecognition.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.Diagnostics;
+using System;
+
+namespace SimpleDiscordSubtitlingBot
+{
+    public class SpeechRecognition
+    {
+        private SpeechRecognizer _recognizer;
+        private PushAudioInputStream _pushStream;
+
+        public event Action<string, string> OnTranscriptUpdated;
+        public event Action<string> OnTranscriptCleared;
+
+        public string Username { get; private set; }
+
+        public SpeechRecognition(string subscriptionKey, string serviceRegion, string username)
+        {
+            var speechConfig = SpeechConfig.FromSubscription(subscriptionKey, serviceRegion);
+            speechConfig.SpeechRecognitionLanguage = "en-US";
+            speechConfig.SetProfanity(ProfanityOption.Raw);
+
+            Username = username;
+            _pushStream = AudioInputStream.CreatePushStream();
+            _recognizer = new SpeechRecognizer(speechConfig, AudioConfig.FromStreamInput(_pushStream));
+
+            _recognizer.Recognized += (s, e) =>
+            {
+                if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Debug.WriteLine($"{Username}: {e.Result.Text}");
+                    OnTranscriptUpdated?.Invoke(Username, e.Result.Text);
+                }
+            };
+
+            _recognizer.Recognizing += (s, e) =>
+            {
+                OnTranscriptUpdated?.Invoke(Username, e.Result.Text);
+            };
+        }
+
+        public void UpdateUsername(string newUsername)
+        {
+            Username = newUsername;
+        }
+
+        public void StartRecognition() => _recognizer.StartContinuousRecognitionAsync();
+
+        public void StopRecognition()
+        {
+            _recognizer.StopContinuousRecognitionAsync().Wait();
+            _pushStream.Close();
+        }
+
+        public void ProcessAudio(byte[] audioData) => _pushStream.Write(audioData);
+    }
+}
